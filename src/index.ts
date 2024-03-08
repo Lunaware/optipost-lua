6a81abb9067b4355131e3c598bf58aa2066800da
Luna <52232518+Lunaware@users.noreply.github.com>
2024-03-08 18:27:39 +1300

    Update .prettierrc and package.json

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,10 @@
  *	@author methamphetqmine
  */
 
-import {} from "@rbxts/services";
+import { HttpService } from "@rbxts/services";
 import { Trove } from "@rbxts/trove";
-import { HttpRequest, HttpQueue, HttpResponse } from "@rbxts/http-queue";
+import { HttpRequest, HttpQueue, HttpResponse, HttpRequestPriority } from "@rbxts/http-queue";
+import Signal from "@rbxts/goodsignal";
 
 const OptipostHttpPool = new HttpQueue({
 	retryAfter: { cooldown: 5 },
@@ -17,31 +18,80 @@ const OptipostHttpPool = new HttpQueue({
 class Optipost {
 	Authorization: undefined | string;
 	baseUrl: string;
-
 	PlaceId: number = game.PlaceId;
 	JobId: string = game.JobId;
-
 	isConnected: boolean = false;
+	queryTime: number = 30;
 
 	Trove: Trove = new Trove();
+	OnJobRecieved: Signal = new Signal();
 
 	constructor(baseUrl: string, Authorization: string | undefined) {
 		this.Authorization = Authorization || undefined;
 		this.baseUrl = baseUrl;
 	}
 
-	public Connect(): HttpRequest {
-		const request: HttpRequest = new HttpRequest(`${this.baseUrl}/connect`, "POST", undefined, undefined);
+	/**
+	 * Connects to the server using a POST request and returns a promise that resolves to an HttpResponse.
+	 * If the request is successful and the response status code is 200, sets the `isConnected` property to true.
+	 * @returns A promise that resolves to an HttpResponse.
+	 */
+	public Connect(): Promise<HttpResponse> {
+		const headers = this.Authorization ? { Authorization: this.Authorization } : undefined;
 
-		OptipostHttpPool.Push(request).then((response: HttpResponse) => {
-			if (response) {
-				this.isConnected = true;
-			}
-		});
+		const request: HttpRequest = new HttpRequest(
+			`${this.baseUrl}/connect`,
+			"POST",
+			HttpService.JSONEncode({ JobId: this.JobId }),
+			undefined,
+			headers,
+		);
 
-		return request;
+		return OptipostHttpPool.Push(request, HttpRequestPriority.First).andThen(
+			(response: HttpResponse): HttpResponse => {
+				if (response.RequestSuccessful === true && response.StatusCode === 200) {
+					this.isConnected = true;
+				}
+
+				return response;
+			},
+		);
 	}
 
+	/**
+	 * Fetches the jobs from the server.
+	 * @returns A promise that resolves to an HttpResponse object.
+	 * @throws {Error} If Optipost is not connected to the server.
+	 */
+	private FetchJobs(): Promise<HttpResponse> {
+		assert(this.isConnected, "Optipost is not connected to the server.");
+
+		const headers = this.Authorization ? { Authorization: this.Authorization } : undefined;
+
+		const request: HttpRequest = new HttpRequest(
+			`${this.baseUrl}/jobs/${this.JobId}`,
+			"GET",
+			undefined,
+			undefined,
+			headers,
+		);
+
+		return OptipostHttpPool.Push(request, HttpRequestPriority.Normal).andThen(
+			(response: HttpResponse): HttpResponse => {
+				if (response.StatusCode === 404) {
+					this.isConnected = false;
+				} else if (response.StatusCode === 200) {
+					this.OnJobRecieved.Fire(response.Body);
+				}
+
+				return response;
+			},
+		);
+	}
+
+	/**
+	 * Destroys the object and cleans up any resources associated with it.
+	 */
 	Destroy(): void {
 		this.isConnected = false;
 		this.Trove.destroy();
